587efd17f4f3f851791c143c1603d2eeabf480e1
Jeremy Loye <jeremyloye@gmail.com>
2018-07-30 01:39:53 +0800

    Edited styling, added user competition listings

--- Project_Website/Public/form/ViewCompetition.js
+++ Project_Website/Public/form/ViewCompetition.js
@@ -1,14 +1,68 @@
 'use strict';
 
 const firestore = firebase.firestore();
-const settings = {/* your settings... */
-  timestampsInSnapshots: true
+const settings = { /* your settings... */
+    timestampsInSnapshots: true
 };
 
-firestore.settings(settings);
+firestore.settings(settings)
 
-firebase.firestore().collection('compDetails').orderBy('date').limit(50).get().then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-        console.log(doc.id, " => ", doc.data());
-    });
-});
+firestore.collection('compDetails').orderBy('date').limit(50).get().then(function (querySnapshot) {
+    let user = firebase.auth().currentUser
+    querySnapshot.forEach(function (doc) {
+        let data = doc.data()
+        let register
+        if (user.displayName) {
+            register = '<a onclick="register(user.uid, data.compName)" href="">Register</a>'
+        } else {
+            register = '<p>Registered</p>'
+        }
+        $(document).ready(() => {
+            $('.listing').append('<div class="demo-card-wide mdl-card mdl-shadow--2dp">' +
+                '<div class="mdl-card__title">' +
+                '<h2 class="mdl-card__title-text">' + data.compName + '</h2>' +
+                '</div>' +
+                '<div class="mdl-card__supporting-text">' +
+                '<p>' + data.details + '</p>' +
+                '</div>' +
+                '<div class="mdl-card__actions mdl-card--border">' +
+                register +
+                '</div>')
+        })
+    })
+})
+
+function register(uid, compName) {
+    let docRef = firestore.collection('userDetails').doc(uid)
+    let data = docRef.get().data()
+    data.compsInvolved.push(compName)
+    firestore.runTransaction(transaction => {
+        return transaction.get(docRef).then(doc => {
+            if (!doc.exists) {
+                throw 'Document does not exist'
+            }
+            transaction.update(docRef, data)
+            return data
+        })
+    }).then(data => {
+        console.log('New Data: ', data)
+    }).catch(error => {
+        console.log(error)
+    })
+    docRef = firestore.collection('compDetails').doc(compName)
+    data = docRef.get().data()
+    data.users.push(compName)
+    firestore.runTransaction(transaction => {
+        return transaction.get(docRef).then(doc => {
+            if (!doc.exists) {
+                throw 'Document does not exist'
+            }
+            transaction.update(docRef, data)
+            return data
+        })
+    }).then(data => {
+        console.log('New Data: ', data)
+    }).catch(error => {
+        console.log(error)
+    })
+}
