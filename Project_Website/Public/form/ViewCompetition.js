af598176cb61a3f6d9427b136736033e86052ed0
Jeremy Loye <jeremyloye@gmail.com>
2018-07-30 15:44:28 +0800

    Made card layout, cleaned up some details

--- Project_Website/Public/form/ViewCompetition.js
+++ Project_Website/Public/form/ViewCompetition.js
@@ -1,19 +1,28 @@
 'use strict';
 
-const firestore = firebase.firestore();
+const firestore = firebase.firestore()
 const settings = { /* your settings... */
     timestampsInSnapshots: true
 };
+
+const storage = firebase.storage()
 
 firestore.settings(settings)
 
 function viewCards(userData) {
     firestore.collection('compDetails').orderBy('date').limit(50).get().then(function (querySnapshot) {
         let user = firebase.auth().currentUser
+        let cardNum = 0
         querySnapshot.forEach(function (doc) {
+            let cardId = 'card' + cardNum
             let data = doc.data()
             let compName = data.compName
             let register
+            let storageRef = storage.refFromURL(`gs://sgcomps-b8fdc.appspot.com/${data.photoPath}`)
+            storageRef.getDownloadURL().then(url => {
+                console.log(url)
+                $(`#${cardId}`).css('background', 'url(' + url + ')')
+            })
             if (userData.orgName) {
                 register = ''
             } else if (data.participants.indexOf(user.uid) == -1) {
@@ -24,18 +33,26 @@ function viewCards(userData) {
 
             $(document).ready(() => {
                 $('.listing').append('<div class="demo-card-wide mdl-card mdl-shadow--2dp">' +
-                    '<div class="mdl-card__title">' +
+                    `<div class="mdl-card__title" id="${cardId}">` +
+                    `<p class='competitionTag'>${data.comp_type}</p>` +
                     '<h2 class="mdl-card__title-text">' + data.compName + '</h2>' +
                     '</div>' +
                     '<div class="mdl-card__supporting-text">' +
                     '<p>' + data.details + '</p>' +
+                    `<p>When: ${data.date}</p>` +
                     '</div>' +
                     '<div class="mdl-card__actions mdl-card--border">' +
                     register +
-                    '</div>' +
-                    $('.mdl-card__title').css('background', 'url(' + data.photoPath + ')')
+                    '</div>' + '<style>' +
+                    `#${cardId} {` +
+                    'color: #fff;' +
+                    'height: 176px;' +
+                    `background: url("");` +
+                    'background-size: cover;' +
+                    '} </style>'
                 )
             })
+            cardNum += 1
         })
     })
 }
--- Project_Website/Public/form/myComps.js
+++ Project_Website/Public/form/myComps.js
@@ -5,23 +5,41 @@ const settings = { /* your settings... */
     timestampsInSnapshots: true
 };
 
+const storage = firebase.storage()
+
 firestore.settings(settings)
 
 function generateList(uid) {
     firestore.collection('userDetails').doc(uid).get().then(doc => {
         let compsInvolved = doc.data().compsInvolved
+        let cardNum = 0
         for (let i = 0; i < compsInvolved.length; i++) {
             firestore.collection('compDetails').doc(compsInvolved[i]).get().then(doc => {
+                let cardId = 'card' + cardNum
                 let data = doc.data()
+                let storageRef = storage.refFromURL(`gs://sgcomps-b8fdc.appspot.com/${data.photoPath}`)
+                storageRef.getDownloadURL().then(url => {
+                    console.log(url)
+                    $(`#${cardId}`).css('background', 'url(' + url + ')')
+                })
                 $(document).ready(() => {
                     $('.listing').append('<div class="demo-card-wide mdl-card mdl-shadow--2dp">' +
-                        '<div class="mdl-card__title">' +
+                        `<div class="mdl-card__title" id="${cardId}">` +
+                        `<p class='competitionTag'>${data.comp_type}</p>` +
                         '<h2 class="mdl-card__title-text">' + data.compName + '</h2>' +
                         '</div>' +
                         '<div class="mdl-card__supporting-text">' +
                         '<p>' + data.details + '</p>' +
-                        '</div>' + '</div>')
+                        `<p>When: ${data.date}</p>` +
+                        '</div>' + '</div>' + '<style>' +
+                        `#${cardId} {` +
+                        'color: #fff;' +
+                        'height: 176px;' +
+                        `background: url("");` +
+                        'background-size: cover;' +
+                        '} </style>')
                 })
+                cardNum += 1
             }).catch(error => {
                 console.log(error)
             })
